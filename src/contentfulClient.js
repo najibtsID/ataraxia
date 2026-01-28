144a16711a9b3d70bdc57c8a742dabcf62b47e6f
Najib Tri Setiawan <08.najibtrisetiawan@gmail.com>
2026-01-28 20:48:48 +0700

    update

--- src/contentfulClient.js
+++ src/contentfulClient.js
@@ -1,4 +1,4 @@
-import { createClient } from 'contentful';
+import { createClient } from "contentful";
 
 const client = createClient({
   space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
@@ -6,8 +6,8 @@ const client = createClient({
 });
 
 export async function fetchCoffees() {
-  const entries = await client.getEntries({ content_type: 'coffees' });
-  return entries.items.map(item => {
+  const entries = await client.getEntries({ content_type: "coffees" });
+  return entries.items.map((item) => {
     const f = item.fields;
     return {
       id: item.sys.id,
@@ -15,7 +15,9 @@ export async function fetchCoffees() {
       description: f.description,
       price: f.price,
       category: f.category,
-      image: f.image?.fields?.file?.url ? ('https:' + f.image.fields.file.url) : null,
+      image: f.image?.fields?.file?.url
+        ? "https:" + f.image.fields.file.url
+        : null,
       featured: !!f.featured,
     };
   });
